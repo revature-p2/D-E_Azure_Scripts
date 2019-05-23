7a323f60a29578d71cd9b0cd2a4a04b69b9781a4
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-23 02:14:03 -0500

    Update createnetwork.sh

--- createnetwork.sh
+++ createnetwork.sh
@@ -9,7 +9,30 @@ publicSubnet=$7
 publicIP=$8
 
 ## conditional for vnet name
-## validation to check if subnet isnt larger than vnet
+vnetcheck=$(az network vnet list -g $username --query [].name | grep -E $vnetName)
+if [ -n $vnetcheck ]; then
+echo "$vnetName already exists"
+exit 1
+fi
+
+## conditional for subnet names
+privatecheck=$(az network vnet list -g $username --query [].subnets[].name | grep -E $privateSubnet)
+if [ -n $privatecheck ]; then
+echo "$privateSubnet already exists"
+exit 1
+fi
+
+publiccheck=$(az network vnet list -g $username --query [].subnets[].name | grep -E $publicSubnet)
+if [ -n $privatecheck ]; then
+echo "$privateSubnet already exists"
+exit 1
+fi
+
+## validation to check if subnet isnt larger than vnet (currently psuedo code)
+if [ $privateIP > $vnetIP ] && [ $publicIP > $vnetIP ]; then
+echo "subnets need to be smaller than the network"
+exit 1
+fi
 
 az network nsg create --name nsg_public -g $username
 
