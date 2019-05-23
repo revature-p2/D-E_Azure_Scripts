58576af622da9a99692cf89ef49a1ecf06f16224
EricBAllen <49167218+EricBAllen@users.noreply.github.com>
2019-05-22 21:15:46 -0500

    Add files via upload

---
+++ NEWcreatedatabase.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+cosmos=$2 # name of database account
+username=$3 # name of resource group
+databaseName=$4 # name of database
+collection=$5 # database collection name
+password=$6 # admin password
+SQLdatabase=$7 # SQL database name
+
+
+case $1 in
+
+nonrelational)
+
+result1=$(az cosmosdb list --query [].name | grep -E /$cosmos/) # database account
+if [ -n "$result1" ]; then
+    echo "$cosmos exists"
+    exit 1
+fi
+az cosmosdb create --name $cosmos -g $username # create database account
+
+
+result2=$(az cosmosdb database list -n $cosmos -g $username --query [].id | grep -E /$databaseName/) # database 
+if [ -n "$result2" ]; then
+    echo "$databaseName exists"
+    exit 1
+fi
+az cosmosdb database create --db-name $databaseName --name $cosmos -g $username # create database
+
+
+result3=$(az cosmosdb collection list -d $databaseName -g $username -n $cosmos --query [].id | grep -E /$collection/)
+if [ -n "$result3" ]; then
+    echo "$collection exists"
+    exit 1
+fi
+az cosmosdb collection create --collection-name $collection -g $username --db-name $databaseName --name $cosmos
+;;
+
+relational)
+
+result4=$(az sql server list --query [].name | grep -E /EricDavidsqlserver/) # since EricDavidsqlserver is static
+if [ -n "$result4" ]; then
+    echo "EricDavidsqlserver exists"
+    exit 1
+fi
+# name of server will be all lowercase
+az sql server create --location southcentralus -g $username --name EricDavidsqlserver --admin-user $username --admin-password $password
+
+
+result5=$(az sql db list --server EricDavidsqlserver -g $username --query [].name | grep -E /$SQLdatabase/)
+if [ -n "$result5" ]; then
+    echo "$SQLdatabase exists"
+    exit 1
+fi
+az sql db create -g $username --server EricDavidsqlserver --name $SQLdatabase --max-size 10GB
+
+esac
