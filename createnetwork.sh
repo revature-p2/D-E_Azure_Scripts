f6de5226c7e2769df645be3d32ea0fe5635ed423
EricBAllen <49167218+EricBAllen@users.noreply.github.com>
2019-05-22 23:52:02 -0500

    Update createnetwork.sh

--- createnetwork.sh
+++ createnetwork.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 username=$1
+vnetName=$2
+vnetIP=$3
+vnetMask=$4
+privateSubnet=$5
+privateIP=$6
+publicSubnet=$7
+publicIP=$8
 
 az network nsg create --name nsg_public -g $username
 
 az network nsg create --name nsg_private -g $username
 
 az network nsg rule create --name in_rule --nsg-name nsg_public -g $username --priority 100 --direction inbound --source-address-prefixes 0.0.0.0/1 \
---source-port-ranges 22 80 443 --destination-address-prefixes 100.0.0.0/25 --destination-port-ranges 22 80 443 --protocol '*' 
+--source-port-ranges 22 80 443 --destination-address-prefixes $publicIP --destination-port-ranges 22 80 443 --protocol '*' 
 
 
-az network nsg rule create --name out_rule --nsg-name nsg_public -g $username --priority 100 --direction outbound --source-address-prefixes 100.0.0.0/25 \
+az network nsg rule create --name out_rule --nsg-name nsg_public -g $username --priority 100 --direction outbound --source-address-prefixes $publicIP \
  --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges '*' --protocol '*'
 
 
-az network nsg rule create --name in_rule --nsg-name nsg_private -g $username --priority 100 --direction inbound --source-address-prefixes 100.0.0.0/25 \
+az network nsg rule create --name in_rule --nsg-name nsg_private -g $username --priority 100 --direction inbound --source-address-prefixes $publicIP \
 --source-port-ranges 22 --destination-address-prefixes '*' --destination-port-ranges 22 --protocol '*'
 
 
-az network nsg rule create --name out_rule --nsg-name nsg_private -g $username --priority 100 --direction outbound --source-address-prefixes 100.0.0.128/25 \
+az network nsg rule create --name out_rule --nsg-name nsg_private -g $username --priority 100 --direction outbound --source-address-prefixes $privateIP \
  --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges '*' --protocol '*'
 
-az network vnet create --name virutalnetwork -g $username --address-prefixes 100.0.0.0/24 --location southcentralus
+az network vnet create --name $vnetName -g $username --address-prefixes $vnetIP --location southcentralus
 
-az network vnet subnet create --address-prefixes 100.0.0.0/25 --vnet-name virtualnetwork -n subnet_public -g $username --network-security-group nsg_public
+az network vnet subnet create --address-prefixes $publicIP --vnet-name $vnetName -n $publicSubnet -g $username --network-security-group nsg_public
 
-az network vnet subnet create --address-prefixes 100.0.0.128/25 --vnet-name virtualnetwork -n subnet_private -g $username --network-security-group nsg_private
+az network vnet subnet create --address-prefixes $privateIP --vnet-name $vnetName -n $privateSubnet -g $username --network-security-group nsg_private
