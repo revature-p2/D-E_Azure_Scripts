7c5e2e2e8577ec0d52b201f54c80babff5f072fc
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-23 23:44:30 -0500

    Update createroles.sh

--- createroles.sh
+++ createroles.sh
@@ -5,13 +5,13 @@ upn=$1@$DOMAIN
 action=$2
 role=$3
 
-userCheck=$(az ad user list --query [].userPrincipalName | grep -E /$upn/)
+userCheck=$(az ad user list --query [].userPrincipalName | grep -E $upn)
 
 ## make/remove role
 
 if [ -z "$userCheck" ]; then
 echo "no user"
-exit 1
+exit 0
 fi
 
 az role assignment $action --role $role --assignee $upn
