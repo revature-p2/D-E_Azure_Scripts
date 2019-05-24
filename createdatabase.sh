5f393d4629ebd605eeb68285e4c738f97db386f9
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-23 23:43:07 -0500

    Update createdatabase.sh

--- createdatabase.sh
+++ createdatabase.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-username=$1
-server=$2
-user=$3
-password=$4
-SQLdatabase=$5
+username=yoyo
+server=$1
+user=$2
+password=$3
+SQLdatabase=$4
 
-result=$(az sql server list --query [].name | grep -E $server) # since sqlserver is static
-result2=$(az sql db list --server $server -g $username --query [].name | grep -E $SQLdatabase)
+result=$(az sql server list -g $username --query [].name | grep -E $server)
 
 if [ -n "$result" ]; then
     echo "$server exists"
-    exit 1
+    exit 0
 fi
 
-# name of server will be all lowercase
-if [ -n "$result2" ]; then
-    echo "$SQLdatabase exists"
-    exit 1
-fi
 
 az sql server create --location southcentralus -g $username --name $server \
  --admin-user $user --admin-password $password
+
+result2=$(az sql db list --server $server -g $username --query [].name | grep -E $SQLdatabase)
+# name of server will be all lowercase
+if [ -n "$result2" ]; then
+    echo "$SQLdatabase exists"
+    exit 0
+fi
+
+
 az sql db create -g $username --server $server --name $SQLdatabase --max-size 10GB
