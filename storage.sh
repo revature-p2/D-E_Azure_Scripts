a0a0c5ba829098abd586b2f3135640f1973d2f6b
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-23 23:48:29 -0500

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-username=$1
-storagename=$2
-container=$3
+username=yoyo
+storagename=$1
+container=$2
 
 
 storagecheck=$(az storage account list -g $username --query [].name | grep -E $storagename)
-if [ -n $storagecheck ]; then
+if ! [ -z $storagecheck ]; then
 echo "$storagename already exists"
-exit 1
-fi
-
-containercheck=$(az storage container list --account-name $storagename --query [].name | grep -E $container)
-if [ -n $containercheck ]; then
-echo "$container already exists"
+exit 0
 fi
 
 az storage account create -n $storagename -g $username --sku Standard_LRS --encryption blob
 
+containercheck=$(az storage container list --account-name $storagename --query [].name | grep -E $container)
+if ! [ -z $containercheck ]; then
+echo "$container already exists"
+fi
+
 az storage container create -n $container --account-name $storagename --public-access blob
+
