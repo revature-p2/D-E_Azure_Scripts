fdfa2fa5999b3966d884efb21d1b9d734060835d
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-23 23:47:40 -0500

    Update createvm.sh

--- createvm.sh
+++ createvm.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-username=$1
-vmname=$2
-network=$3
-vmsubnet=$4
-user=$5
-password=$6
+username=yoyo
+vmname=$1
+network=$2
+vmsubnet=$3
+user=$4
+password=$5
 
 
 vnetcheck=$(az network vnet list -g $username --query [].name | grep -E $network)
 if [ -z $vnetcheck ]; then
 echo "$network doesn't exists"
-exit 1
+exit 0
 fi
 
 vmcheck=$(az vm list -g $username --query [].name | grep -E $vmname)
-if [ -n $vmcheck]; then
+if ! [ -z $vmcheck]; then
 echo "$vmname already exists"
-exit 1
+exit 0
 fi
 
 
-## az vm list -g yoyo -d --query [].publicIps
+az vm create --name $vmname -g $username --image UbuntuLTS --location southcentralus --size Standard_B1ls --admin-username $user \
+--admin-password $password --vnet-name $network --subnet $vmsubnet
 
-az vm create --name $vmname --resource-group $username --image UbuntuLTS --location southcentralus --size B1s --admin-username $user \
---admin-password $password --vnet-name $network --subnet $vmsubnet --no-wait
-echo Creating your Virtual Machine
-echo $ipaddress
-exit 0
+ipaddress=$(az vm list -d --query "[?name=='$vmname'].publicIps")
+
+echo connect to your vm with $ipaddress
 
