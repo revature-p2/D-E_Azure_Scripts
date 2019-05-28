7ef89b2d65e81daea52f7beaaa8c4987a1b3e1d5
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-27 22:18:20 -0500

    Delete createapp.sh

--- createapp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-username=$1
-gitrepo=$2
-
-az appservice plan create --name serviceplan -g $username --sku F1 --location southcentralus --is-linux
-az webapp create -g $username --plan serviceplan --name application -r "node|10.14"
-az webapp deployment source config --name application -g $username --repository-type github --repo-url $gitrepo --branch master
